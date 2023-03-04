01321ae1f73b56a1b0eade9ba7c62f5270f8a816
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-03-05 03:35:30 +0530

    icons and dependencies added

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { IoAddCircleOutline } from "react-icons/io";
 
 function FirstPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center">Hello world!</h1>
+    <div className="">
+      <div className="bg-cover border border-black bg-center ">
+        <img
+          src="https://images.unsplash.com/photo-1634270874854-3c7492905b74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHB1cnBsZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+          className="w-full h-[200px]"
+        />
+        <IoAddCircleOutline />
+      </div>
     </div>
   );
 }
