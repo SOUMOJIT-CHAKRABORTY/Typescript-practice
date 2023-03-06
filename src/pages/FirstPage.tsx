ffa5d14c5b99dbae67f8a92edd655ee8dae7f295
Soumojit Chakraborty <goutamghambir2018@gmail,com>
2023-03-06 12:01:49 +0530

    merging branches

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { IoAddCircleOutline } from "react-icons/io";
+import { BiCommentAdd } from "react-icons/bi";
 
 function FirstPage() {
   return (
-    <div className="">
-      <div className="bg-cover border border-black bg-center ">
-        <img
-          src="https://images.unsplash.com/photo-1634270874854-3c7492905b74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHB1cnBsZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-          className="w-full h-[200px]"
-        />
-        <IoAddCircleOutline />
-      </div>
+    <div className="item-center justify-center ">
+    <button className="flex flex-row item-center p-3 justify-center mt-10 cursor-pointer  hover:bg-slate-600 mx-auto rounded-xl active:bg-slate-700">
+      <BiCommentAdd className="text-4xl "/>
+      <h1 className="font-bold text-lg"> Add Todo</h1>
+    </button>
+
+    <div className=" justify-center mx-auto w-full md:w-[67%] lg:w-[40%] lg:ml-20 space-x-5">
+      <form className="mx-auto p-6 flex flex-col justify-center ">
+        <input name="todotitle" placeholder="title" type="text" className="border border-slate-500 mb-5 rounded-md py-1 px-4 w-[50%]"/>
+        <textarea placeholder="Add details" />
+      </form>
+    </div>
     </div>
   );
 }
