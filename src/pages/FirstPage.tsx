513c6785b4275f6ea248086ee0e193545d782afe
Soumojit Chakraborty <goutamghambir2018@gmail,com>
2023-03-07 02:20:22 +0530

    todo format is created

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BiCommentAdd } from "react-icons/bi";
 
 function FirstPage() {
@@ -9,11 +8,11 @@ function FirstPage() {
       <h1 className="font-bold text-lg"> Add Todo</h1>
     </button>
 
-    <div className=" justify-center mx-auto w-full md:w-[67%] lg:w-[40%] lg:ml-20 space-x-5">
-      <form className="mx-auto p-6 flex flex-col justify-center ">
-        <input name="todotitle" placeholder="title" type="text" className="border border-slate-500 mb-5 rounded-md py-1 px-4 w-[50%]"/>
-        <textarea placeholder="Add details" />
-      </form>
+    <div className="flex justify-center items-center ">
+
+        <input name="todotitle" placeholder="title" type="text" className="border border-slate-500 rounded-md py-1 px-4 "/>
+        <button className="ml-1 bg-blue-700 px-3 py-1 rounded-lg text-white" >Add</button>
+
     </div>
     </div>
   );
