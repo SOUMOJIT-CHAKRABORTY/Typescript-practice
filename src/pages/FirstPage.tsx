29bb5b4e4a45714ba13743f7567bc8195dd39124
Soumojit Chakraborty <goutamghambir2018@gmail,com>
2023-03-09 02:29:18 +0530

    todo is completed except delete option

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,7 +1,41 @@
+import { FC, useState } from "react";
 import { BiCommentAdd } from "react-icons/bi";
+import { Todo } from "./Todos";
 
-function FirstPage() {
-  return (
+const FirstPage:FC = ()=>{
+
+  const [todo,setTodo] = useState<Todo[]>([]);
+  
+
+  const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo ={
+      id: todo.length + 1,
+      text: todoText,
+      completed: false
+
+    }
+    setTodo([...todo,newTodo])
+    setTodoText('')
+  }
+  const [todoText , setTodoText] = useState<string>('');
+
+  const handleTogle = (id:number) => {
+    const updateTodos = todo.map((todo)=> {
+      if(todo.id===id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    })
+    setTodo(updateTodos);
+  } 
+
+
+
+  return <>
     <div className="item-center justify-center ">
     <button className="flex flex-row item-center p-3 justify-center mt-10 cursor-pointer  hover:bg-slate-600 mx-auto rounded-xl active:bg-slate-700">
       <BiCommentAdd className="text-4xl "/>
@@ -9,13 +43,25 @@ function FirstPage() {
     </button>
 
     <div className="flex justify-center items-center ">
+    <form onSubmit={onSubmit}>
+        <input name="todotitle" placeholder="title" value={todoText} type="text" onChange={(e)=> setTodoText(e.currentTarget.value)} className="border border-slate-500 rounded-md py-1 px-4 "/>
+        <button type="submit" className="ml-1 bg-blue-700 px-3 py-1 rounded-lg text-white" >Add</button>
 
-        <input name="todotitle" placeholder="title" type="text" className="border border-slate-500 rounded-md py-1 px-4 "/>
-        <button className="ml-1 bg-blue-700 px-3 py-1 rounded-lg text-white" >Add</button>
+    </form>
+    </div>
+      {
+        todo.map((todo)=> {
+          return(
+          <div key={todo.id} className='flex flex-row justify-center mt-5 space-x-3 font-light border-2 rounded-md border-gray-300 p-5 mx-10'>
+            <input type='checkbox' checked={todo.completed} onChange={() =>handleTogle(todo.id)} />
+            <span className="text-center">{todo.text}</span>
 
+          </div>
+          )
+        })
+      }
     </div>
-    </div>
-  );
+    </>
 }
 
 export default FirstPage;
